4dcda70f37d862176494caff6bfbd2201ecc348d
Rybnikov Vadim <va6d6@mail.ru>
2019-11-11 01:46:31 +0500

    Добавлен обработчик картинок, изменены структура, добавлен раздел static, отслеживание изменений в index.html

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,14 +1,28 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const { VueLoaderPlugin } = require('vue-loader')
+
+const PATHS = {
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist'),
+    assets: 'assets/'
+}
 
 module.exports = {
+
+    externals:{
+        paths: PATHS
+    },
+
     entry: {
-        app: './src/index.js'
+        app: PATHS.src
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        publicPath: "/dist"
+        path: PATHS.dist,
+        filename: `${PATHS.assets}js/[name].js`,
+        publicPath: "/"
     },
     module:{
         rules: [
@@ -16,6 +30,13 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: '/node_modules/'
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options:{
+                    name: '[name].[ext]'
+                }
             },
             {
                 test: /\.css$/,
@@ -69,12 +90,24 @@ module.exports = {
         ]
     },
 
-    devServer:{
-        overlay: true
-    },
+
     plugins:[
         new MiniCssExtractPlugin({
-            filename: "[name].css"
-        })
+            filename: `${PATHS.assets}css/[name].css`,
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATHS.src}/index.html`,
+            filename: './index.html'
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: `${PATHS.src}/img`,
+                to: `${PATHS.assets}img`
+            },{
+                from: `${PATHS.src}/static`,
+                to: ''
+            }
+        ])
     ],
 };
